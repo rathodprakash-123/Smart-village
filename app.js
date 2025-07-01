933bde47a2369abd05f67fb167a5daa1961f7907
Rathod Prakash <rathodprakash7926@gmail.com>
2025-07-01 20:30:52 +0530

    initial

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ const indexRoutes = require("./routes/index");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
 
-
-
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect('mongodb://localhost:27017/smart-village')
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
@@ -52,5 +50,5 @@ app.use("/", userRoutes);
 app.use("/admin",adminRoutes);
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`)); 
